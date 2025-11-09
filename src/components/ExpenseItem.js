8a3aaf31675c6edbff6f642c0b90353149cdec9d
OtokarSefer <otokar.sefer@voco.ee>
2025-11-09 16:05:52 +0200

    Ülesanne 3. Lõin kaarti componendi ja lisasin selle lehele, et see meenutaks pilti dokumendis

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,11 @@
 import './ExpenseItem.css'
 import ExpenseDate from "./ExpenseDate";
+import Card from "./UI/Card"
 
 function ExpenseItem(props){
 
     return (
-            <div className="expense-item">
+            <div className="card expense-item">
                 <ExpenseDate date={props.expenseData.date}></ExpenseDate>
                 <div className="expense-item__description">
                     <h2>{props.expenseData.title}</h2>
